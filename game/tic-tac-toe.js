1715809a22c17e354d35bf657d125c65b96753af
Romar Plummer <rplummer@atlassian.com>
2019-10-21 23:14:01 -0500

    added

--- game/tic-tac-toe.js
+++ game/tic-tac-toe.js
@@ -1,11 +1,37 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const board = document.getElementById('board').children;
-    var c= ''; 
-    var i;
-    for(i=0; i <board.length; i++){
-        c=board[i].classList.add('square');
-       }
-    document.addEventListener('click', function(){
+let addBoardClass = () =>{
+    let entireBoard= document.getElementById('board');
+    let cells=entireBoard.children;
 
-    });
-});    
+    for (i=0; i<cells.length; i++){
+        cells[i].classList.add('square');
+
+    }
+    
+    for (let j=0; j<cells.length; j++){
+        cells[j].onmouseover= function(){cells[j].classList.toggle("hover",true)}
+        cells[j].onmouseout= function(){cells[j].classList.toggle("hover",false)}
+    }
+    for (let i=0; i<cells.length; i++){
+        cells[i].addEventListener("click",function(){
+
+            if (gameState.length==0){
+                cells[i].classList.add("X");
+                cells[i].innerHTML=("X");
+                gameState.push("X");
+            }else{
+                if (gameState[gameState.length-1]=='X'){
+                    cells[i].classList.add("O");
+                    cells[i].innerHTML=("O");
+                    gameState.push("O");
+            }else{
+                cells[i].classList.add("X");
+                cells[i].innerHTML=("X");
+                gameState.push("X");
+
+                }
+            }
+            });
+        }
+    }
+    document.addEventListener('DOMContentLoaded', addBoardClass);
+    var gameState=[];
