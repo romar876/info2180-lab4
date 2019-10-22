593de8648ce1e0bb3be3bd746963766b1321df22
Romar Plummer <rplummer@atlassian.com>
2019-10-21 20:54:10 -0500

    added

---
+++ game/tic-tac-toe.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const board = document.getElementById('board').children;
+    var c= ''; 
+    var i;
+    for(i=0; i <board.length; i++){
+        c=board[i].classList.add('square');
+       }
+    document.addEventListener('click', function(){
+
+    });
+});    
