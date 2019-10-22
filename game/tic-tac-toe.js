e974b2e68323f4905b25b379056c29839abe8167
Romar Plummer <rplummer@atlassian.com>
2019-10-21 23:32:12 -0500

    added

--- game/tic-tac-toe.js
+++ game/tic-tac-toe.js
@@ -2,8 +2,8 @@ let addBoardClass = () =>{
     let entireBoard= document.getElementById('board');
     let cells=entireBoard.children;
 
-    for (i=0; i<cells.length; i++){
-        cells[i].classList.add('square');
+    for (o=0; o<cells.length; o++){
+        cells[o].classList.add('square');
 
     }
     
@@ -11,22 +11,22 @@ let addBoardClass = () =>{
         cells[j].onmouseover= function(){cells[j].classList.toggle("hover",true)}
         cells[j].onmouseout= function(){cells[j].classList.toggle("hover",false)}
     }
-    for (let i=0; i<cells.length; i++){
-        cells[i].addEventListener("click",function(){
+    for (let o=0; o<cells.length; o++){
+        cells[o].addEventListener("click",function(){
 
             if (gameState.length==0){
-                cells[i].classList.add("X");
-                cells[i].innerHTML=("X");
-                gameState.push("X");
+                cells[o].classList.add("X");
+                cells[o].innerHTML=("X");
+                game.push("X");
             }else{
                 if (gameState[gameState.length-1]=='X'){
-                    cells[i].classList.add("O");
-                    cells[i].innerHTML=("O");
-                    gameState.push("O");
+                    cells[o].classList.add("O");
+                    cells[o].innerHTML=("O");
+                    game.push("O");
             }else{
-                cells[i].classList.add("X");
-                cells[i].innerHTML=("X");
-                gameState.push("X");
+                cells[o].classList.add("X");
+                cells[o].innerHTML=("X");
+                game.push("X");
 
                 }
             }
@@ -34,4 +34,4 @@ let addBoardClass = () =>{
         }
     }
     document.addEventListener('DOMContentLoaded', addBoardClass);
-    var gameState=[];
+    var game=[];
